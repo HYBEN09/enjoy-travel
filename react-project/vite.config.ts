0750fe5179778894b722eca425709d352dd32ec6
hyebeen byun <hyebeen2658@naver.com>
2023-04-18 12:16:55 +0900

    [setting]  초기 세팅 수정 - 스타일컴포넌트 설치

--- react-project/vite.config.ts
+++ react-project/vite.config.ts
@@ -1,60 +1,18 @@
-import path from "path";
-import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
-import imageminSvgo from "imagemin-svgo";
-import imageminWebp from "imagemin-webp";
-import react from "@vitejs/plugin-react-swc";
-import imageminMozjpeg from "imagemin-mozjpeg";
-import imageminPngQuant from "imagemin-pngquant";
-import imageminGifSicle from "imagemin-gifsicle";
-import viteImagemin from "@vheemstra/vite-plugin-imagemin";
+import path from 'path';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve("src") }],
+    alias: [{ find: '@', replacement: path.resolve('src') }],
   },
-  define: {
-    "process.env": {},
-  },
-  plugins: [
-    react(),
-    svgr(),
-    viteImagemin({
-      plugins: {
-        jpg: imageminMozjpeg(),
-        png: imageminPngQuant(),
-        gif: imageminGifSicle(),
-        svg: imageminSvgo(),
-      },
-      makeWebp: {
-        plugins: {
-          jpg: imageminWebp(),
-          png: imageminWebp(),
-        },
-      },
-    }),
-  ],
+  plugins: [react(), svgr()],
   server: {
-    host: "localhost",
+    host: 'localhost',
     port: 3000,
   },
   css: {
     devSourcemap: true,
-  },
-
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          reactRouter: ["react-router-dom"],
-          axios: ["axios"],
-          fbApp: ["@firebase/app"],
-          fbAuth: ["@firebase/auth"],
-          fbStore: ["@firebase/firestore"],
-          fbStorage: ["@firebase/storage"],
-        },
-      },
-    },
   },
 });
