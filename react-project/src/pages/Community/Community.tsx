051687c04da37fecd3b0da076bd4dd12a5f65b95
hyebeen byun <hyebeen2658@naver.com>
2023-04-21 23:43:00 +0900

    [feat] 파이어스토어에 올라간 데이터 받아 렌더링

--- react-project/src/pages/Community/Community.tsx
+++ react-project/src/pages/Community/Community.tsx
@@ -1,59 +1,46 @@
 /* eslint-disable react/no-children-prop */
 import Card from '@/components/Card/Card';
+import { db } from '@/firebase/firestore';
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from '@firebase/firestore';
 import styled from 'styled-components';
-
+import { v4 as uuidv4 } from 'uuid';
 function Community() {
+  const [meetups, setMeetups] = useState([]);
+
+  useEffect(() => {
+    const fetchMeetups = async () => {
+      try {
+        // Firestore에서 meetups 컬렉션의 데이터 가져오기
+        const meetupsSnapshot = await getDocs(collection(db, 'meetups'));
+        const meetupsData = meetupsSnapshot.docs.map((doc) => doc.data());
+        setMeetups(meetupsData);
+      } catch (error) {
+        console.error('Error fetching meetups: ', error);
+      }
+    };
+    fetchMeetups();
+  }, []);
+
   return (
     <>
       <CommunityWrapper>
-        <Card
-          imageUrl={
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
-          }
-          title={'영국 여행'}
-          children={'날씨도 좋은'}
-        />
-        <Card
-          imageUrl={
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
-          }
-          title={'영국 여행'}
-          children={'날씨도 좋은'}
-        />
-        <Card
-          imageUrl={
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
-          }
-          title={'영국 여행'}
-          children={'날씨도 좋은'}
-        />
-        <Card
-          imageUrl={
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
-          }
-          title={'영국 여행'}
-          children={'날씨도 좋은'}
-        />
-        <Card
-          imageUrl={
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
-          }
-          title={'영국 여행'}
-          children={'날씨도 좋은'}
-        />
-        <Card
-          imageUrl={
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
-          }
-          title={'영국 여행'}
-          children={'날씨도 좋은'}
-        />
+        {meetups.map((meetup) => (
+          <Card
+            key={uuidv4()}
+            imageUrl={meetup.photoURL}
+            title={meetup.title}
+            children={meetup.description}
+          />
+        ))}
       </CommunityWrapper>
     </>
   );
 }
 
 const CommunityWrapper = styled.div`
+  padding-left: 8px;
+  margin-left: 8px;
   display: grid;
   grid-template-columns: 1fr 1fr;
   margin-top: 1rem;
