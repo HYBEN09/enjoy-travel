a4615f4051d158e777b9ccc7aa5829fa83510095
hyebeen byun <hyebeen2658@naver.com>
2023-04-21 15:09:23 +0900

    [feat] Card component 임시 적용

---
+++ react-project/src/page/Community/Community.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/no-children-prop */
+import Card from '@/components/Card/Card';
+import styled from 'styled-components';
+
+function Community() {
+  return (
+    <>
+      <CommunityWrapper>
+        <Card
+          imageUrl={
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
+          }
+          title={'영국 여행'}
+          children={'날씨도 좋은'}
+        />
+        <Card
+          imageUrl={
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
+          }
+          title={'영국 여행'}
+          children={'날씨도 좋은'}
+        />
+        <Card
+          imageUrl={
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
+          }
+          title={'영국 여행'}
+          children={'날씨도 좋은'}
+        />
+        <Card
+          imageUrl={
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvIaDZX8_hRBOF1l3AQdK8tM4NXZw1-n5EsQ&usqp=CAU'
+          }
+          title={'영국 여행'}
+          children={'날씨도 좋은'}
+        />
+      </CommunityWrapper>
+    </>
+  );
+}
+
+const CommunityWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-top: 1rem;
+`;
+
+export default Community;
