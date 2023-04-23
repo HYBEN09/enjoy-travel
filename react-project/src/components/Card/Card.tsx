4a9f04d8414102d71753156c9710cd3dcbebd4d8
hyebeen byun <hyebeen2658@naver.com>
2023-04-24 00:13:53 +0900

    [feat] 내용이 42보다 크면 문자열을 자르고 '...'을 추가하는 truncatedChildren 변수 추가

--- react-project/src/components/Card/Card.tsx
+++ react-project/src/components/Card/Card.tsx
@@ -11,20 +11,26 @@ export interface CardProps {
   children: ReactNode;
   imageUrl: string;
   title: string;
+  onClick?: () => void;
 }
 
-const Card: React.FC<CardProps> = ({ title, imageUrl, children }) => {
+const Card: React.FC<CardProps> = ({ title, imageUrl, children, onClick }) => {
+  const truncatedChildren =
+    typeof children === 'string' && children.length > 55
+      ? children.slice(0, 42) + '...'
+      : children;
+
   return (
-    <CardWrapper>
+    <CardWrapper onClick={onClick}>
       <div>
-        <Image src={imageUrl} alt="" />
+        <Image src={imageUrl} alt="리뷰 사진" />
       </div>
       <CardContainer>
         <CardTitle>
           <h3>{title}</h3>
         </CardTitle>
         <CardContent>
-          <p>{children}</p>
+          <p>{truncatedChildren}</p>
         </CardContent>
       </CardContainer>
     </CardWrapper>
