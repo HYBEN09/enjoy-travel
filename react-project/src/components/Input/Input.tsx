589958fc38f001fae020bfcdc318083cfa753e16
hyebeen byun <hyebeen2658@naver.com>
2023-04-30 00:54:06 +0900

    [feat] Input component 수정

--- react-project/src/components/Input/Input.tsx
+++ react-project/src/components/Input/Input.tsx
@@ -1,25 +1,32 @@
 /* eslint-disable no-unused-vars */
 import React, { forwardRef, Ref } from 'react';
-
+import { InputForm } from './InputStyled';
 type InputProps = {
   type: string;
-  placeholder: string;
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  id?: string;
+  className?: string;
+  onKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
 };
 
 export const Input = forwardRef(
   (props: InputProps, ref: Ref<HTMLInputElement>) => {
-    const { type, placeholder, value, onChange } = props;
+    const { type, placeholder, value, onChange, id, className, onKeyPress } =
+      props;
 
     return (
-      <input
+      <InputForm
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={onChange}
         required
         ref={ref}
+        id={id}
+        className={className}
+        onKeyPress={onKeyPress}
       />
     );
   }
