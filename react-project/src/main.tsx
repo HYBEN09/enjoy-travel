2f25eeee3cf10cc56dbbd2163eaf6bd4c0b4110b
hyebeen byun <hyebeen2658@naver.com>
2023-04-29 18:03:23 +0900

    [feat] AuthContextProvider 적용

--- react-project/src/main.tsx
+++ react-project/src/main.tsx
@@ -1,9 +1,9 @@
 import App from './App';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { AuthContextProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <AuthContextProvider>
     <App />
-  </React.StrictMode>
+  </AuthContextProvider>
 );
