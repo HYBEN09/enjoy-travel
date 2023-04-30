47ca9975000d818d482def6e7ebb684904abfd2b
hyebeen byun <hyebeen2658@naver.com>
2023-04-30 22:10:36 +0900

    [feat] MeetupCard component 분리

---
+++ react-project/src/components/MeetupCard/MeetupCard.tsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/no-children-prop */
+import Card from '../Card/Card';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import loading from '/public/assets/loading.svg';
+import { LoadingSpinner } from '@/styles/LoadingStyled';
+import { fetchMeetups } from '../../utils/fetchMeetups';
+export function MeetupCard() {
+  const [meetups, setMeetups] = useState([]);
+  const [selectedMeetup, setSelectedMeetup] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchMeetupsData = async () => {
+      const meetupsData = await fetchMeetups();
+      setMeetups(meetupsData);
+      setIsLoading(false);
+    };
+    fetchMeetupsData();
+  }, []);
+
+  const handleCardClick = (meetupTitle, meetup) => {
+    setSelectedMeetup(meetup);
+    navigate(`/community/${meetupTitle}`);
+  };
+
+  return (
+    <>
+      {isLoading && <LoadingSpinner src={loading} alt="로딩 중" />}
+      {meetups.map((meetup) => (
+        <Card
+          key={meetup.id}
+          imageUrl={meetup.photoURL}
+          title={meetup.title}
+          children={meetup.description}
+          onClick={() => handleCardClick(meetup.title, meetup)}
+        />
+      ))}
+    </>
+  );
+}
