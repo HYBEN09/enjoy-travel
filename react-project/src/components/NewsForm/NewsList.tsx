1f59a7270ff3df64bb7a4f124d5dbd335c11feca
seoohyeon <fltk0825@naver.com>
2023-05-04 01:34:52 +0900

    [feat] 뉴스 스크롤시 렌더링 기능 추가 구현

--- react-project/src/components/NewsForm/NewsList.tsx
+++ react-project/src/components/NewsForm/NewsList.tsx
@@ -5,27 +5,51 @@ import { LoadingSpinner } from '@/styles/LoadingStyled';
 import loadingImg from '/public/assets/loading.svg';
 import { NewsListBlock } from './NewsFormStyled';
 
-const NewsList = () => {
+export default function NewsList() {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
-
+  const [page, setPage] = useState(1);
   const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+
+    console.log('스크롤 이벤트 발생');
+    console.log(clientHeight);
+    console.log(scrollTop);
+    console.log(scrollHeight);
+
+    if (scrollTop + clientHeight >= scrollHeight - 1) {
+      console.log('페이지 끝에 스크롤이 닿았음');
+      setPage((prev) => prev + 1);
+    }
+  };
+
+  const getRandomImageThenSet = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}&page=${page}`
+      );
+      setArticles(response.data.articles);
+    } catch {
+      console.error('fetching error');
+    }
+    setLoading(false);
+  };
+
   useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${API_KEY}`
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
+    console.log('page ? ', page);
+    getRandomImageThenSet();
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
-    fetchData();
   }, []);
 
   // 대기 중일 때
@@ -42,14 +66,13 @@ const NewsList = () => {
     return null;
   }
 
-  // articles 값이 유효할 때
   return (
-    <NewsListBlock>
-      {articles.map((article) => (
-        <NewsItem key={article.url} article={article} />
-      ))}
-    </NewsListBlock>
+    <>
+      <NewsListBlock>
+        {articles?.map((article) => (
+          <NewsItem key={article.url} article={article} />
+        ))}
+      </NewsListBlock>
+    </>
   );
-};
-
-export default NewsList;
+}
