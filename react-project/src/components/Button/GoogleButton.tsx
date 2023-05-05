06a3a2e8979bb090caff3924a50161b86b4ce917
hyebeen byun <hyebeen2658@naver.com>
2023-05-05 22:48:24 +0900

    [feat] Google Button component 구현

---
+++ react-project/src/components/Button/GoogleButton.tsx
@@ -0,0 +1,20 @@
+import { auth } from '@/firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import GoogleLogo from '/public/GoogleLogo.svg';
+import { GoogleAuthProvider, signInWithRedirect } from '@firebase/auth';
+
+export function GoogleButton() {
+  const navigate = useNavigate();
+
+  const handleGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+    navigate('/');
+  };
+
+  return (
+    <button onClick={handleGoogleSignIn}>
+      <img src={GoogleLogo} alt="구글" />
+    </button>
+  );
+}
