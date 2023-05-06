e1f9a3346a1d02145dac3be1f9234d2ef1507f0a
hyebeen byun <hyebeen2658@naver.com>
2023-05-07 01:17:39 +0900

    [feat] github login 연결

---
+++ react-project/src/components/Button/SocialLoginButton/GithubButton.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import githubLogo from '/public/assets/githubLogo.svg';
+import { getAuth, signInWithPopup, GithubAuthProvider } from '@firebase/auth';
+
+export function GithubButton() {
+  const navigate = useNavigate();
+
+  const handleGithubSignIn = async () => {
+    const provider = new GithubAuthProvider();
+    const auth = getAuth();
+
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      navigate('/');
+      console.log(user);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <button onClick={handleGithubSignIn}>
+      <img src={githubLogo} alt="깃허브" />
+    </button>
+  );
+}
