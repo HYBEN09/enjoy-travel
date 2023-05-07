2091cb78e2d6a719ba4437f976cbad60f80a6330
seoohyeon <fltk0825@naver.com>
2023-05-07 12:06:46 +0900

    [fix] css파일 분리

--- react-project/src/pages/Signup/Signup.tsx
+++ react-project/src/pages/Signup/Signup.tsx
@@ -1,24 +1,28 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable no-unused-vars */
 import { auth } from '@/firebase/auth';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 import { useState, useRef } from 'react';
-import { AiOutlineEye } from 'react-icons/ai';
 import { useNavigate } from 'react-router-dom';
 import Button from '@/components/Button/Button';
 import { MdArrowForwardIos } from 'react-icons/md';
-import { AiFillCheckCircle } from 'react-icons/ai';
-import { AiFillEyeInvisible } from 'react-icons/ai';
-import { RiErrorWarningLine } from 'react-icons/ri';
 import { useDocumentTitle } from '@/hooks/useDocumentTitle';
 import { createUserWithEmailAndPassword } from '@firebase/auth';
+
+import {
+  CheckStyle,
+  CheckStyle2,
+  IconWrapper,
+  NotShowingPW,
+  ShowingPW,
+  SignUpContainer,
+  SignUpFooter,
+  SignupWrapper,
+  SocialLogin,
+  StyledLink,
+  WarningStyle,
+  WarningStyle2,
+} from './SignUpStyled';
 import { GoogleButton } from '@/components/Button/SocialLoginButton/GoogleButton';
-import { GithubButton } from '@/components/Button/SocialLoginButton/GithubButton';
 import { FaceBookButton } from '@/components/Button/SocialLoginButton/FaceBookButton';
+import { GithubButton } from '@/components/Button/SocialLoginButton/GithubButton';
 
 type SignupFormData = {
   username: string;
@@ -179,138 +183,5 @@ const SignupForm = () => {
     </SignupWrapper>
   );
 };
-
-const CheckStyle = styled(AiFillCheckCircle)`
-  position: absolute;
-  right: 5%;
-  top: 232%;
-  transform: translateY(-50%);
-  color: green;
-`;
-
-const WarningStyle = styled(RiErrorWarningLine)`
-  position: absolute;
-  right: 5%;
-  top: 232%;
-  transform: translateY(-50%);
-  color: red;
-`;
-
-const CheckStyle2 = styled(AiFillCheckCircle)`
-  position: absolute;
-  right: 5%;
-  top: 232%;
-  transform: translateY(-50%);
-  color: green;
-`;
-
-const WarningStyle2 = styled(RiErrorWarningLine)`
-  position: absolute;
-  right: 5%;
-  top: 232%;
-  transform: translateY(-50%);
-  color: red;
-`;
-
-const ShowingPW = styled(AiOutlineEye)`
-  position: absolute;
-  right: 4%;
-  top: 232%;
-  transform: translateY(-50%);
-  font-size: 1.125rem;
-  color: var(--gray-700);
-`;
-
-const NotShowingPW = styled(AiFillEyeInvisible)`
-  position: absolute;
-  right: 4%;
-  top: 232%;
-  transform: translateY(-50%);
-  font-size: 1.125rem;
-  color: var(--gray-700);
-`;
-
-const SignupWrapper = styled.div`
-  background: url('/public/assets/SignUpBg.png') no-repeat;
-  background-size: 100% 270px;
-  padding-top: 250px;
-  position: relative;
-  margin-bottom: 2rem;
-
-  h2 {
-    display: flex;
-    justify-content: center;
-    padding: 30px 0;
-  }
-
-  label {
-    position: relative;
-    margin-bottom: 7px;
-    font-size: 15px;
-  }
-
-  input {
-    padding: 10px;
-    padding-left: 25px;
-    margin-bottom: 20px;
-    border-radius: 100px;
-    box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.3);
-    font-size: 1rem;
-  }
-  input::placeholder {
-    color: var(--gray-600);
-    font-size: 0.7rem;
-  }
-`;
-
-const SignUpContainer = styled.form`
-  display: flex;
-  flex-direction: column;
-  border-top: 1px solid var(--purple-600);
-  border-top-left-radius: 30px;
-  border-top-right-radius: 30px;
-  padding-bottom: 20px;
-  background: var(--white);
-  padding: 0 30px;
-`;
-
-const IconWrapper = styled.span`
-  vertical-align: middle;
-  padding-left: 10px;
-  font-size: 14px;
-`;
-
-const SignUpFooter = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  span {
-    font-size: 10px;
-    display: flex;
-    justify-content: center;
-    padding: 30px 0;
-    font-weight: 600;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  color: var(--accent);
-  &:focus,
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const SocialLogin = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-
-  img {
-    height: 50px;
-    box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.3);
-    border-radius: 100%;
-    padding: 15px;
-  }
-`;
 
 export default SignupForm;
