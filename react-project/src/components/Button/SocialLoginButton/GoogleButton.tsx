8c8646f1488ab96af81ccb043e31733c86868393
seoohyeon <fltk0825@naver.com>
2023-05-08 23:56:07 +0900

    [fix] 구글 로그인후 헤더 UI 수정

--- react-project/src/components/Button/SocialLoginButton/GoogleButton.tsx
+++ react-project/src/components/Button/SocialLoginButton/GoogleButton.tsx
@@ -1,15 +1,21 @@
 import { auth } from '@/firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import GoogleLogo from '/public/assets/GoogleLogo.svg';
-import { GoogleAuthProvider, signInWithRedirect } from '@firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
 
 export function GoogleButton() {
   const navigate = useNavigate();
 
   const handleGoogleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithRedirect(auth, provider);
-    navigate('/');
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log('구글 로그인', result.user);
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
