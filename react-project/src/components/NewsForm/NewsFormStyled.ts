e129c0883a2d84638e2ba82ac4cf7264c5c741a7
hyebeen byun <hyebeen2658@naver.com>
2023-05-09 13:21:13 +0900

    [fix] 오타 수정

--- react-project/src/components/NewsForm/NewsFormStyled.ts
+++ react-project/src/components/NewsForm/NewsFormStyled.ts
@@ -1,35 +1,35 @@
 import styled from 'styled-components';
 
 export const NewsItemBlock = styled.div`
-    display: flex;
-    .thumbnail {
-        margin-right: 1rem;
-        img{
-            dispaly:block;
-            width: 30rem;
-            height: 150px;
-            object-fit:cover;
-        }
+  display: flex;
+  .thumbnail {
+    margin-right: 1rem;
+    img {
+      display: block;
+      width: 30rem;
+      height: 150px;
+      object-fit: cover;
     }
-    .contents {
-        h2 {
-            margin : 0;
-            a{
-                color :black;
-            }
-        }
+  }
+  .contents {
+    h2 {
+      margin: 0;
+      a {
+        color: black;
+      }
     }
-    p{
-        margin : 0;
-        line-height:1.5;
-        margin-top: 0.5rem;
-        white-space:normal;
-        font-size:12px;
-    }
-}
-& + & {
-    margin-top:3rem;
-}`;
+  }
+  p {
+    margin: 0;
+    line-height: 1.5;
+    margin-top: 0.5rem;
+    white-space: normal;
+    font-size: 12px;
+  }
+  & + & {
+    margin-top: 3rem;
+  }
+`;
 
 export const NewsListBlock = styled.div`
   box-sizing: border-box;
