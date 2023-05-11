c9f9f7f92222d0c4ee5fbc4a11c3b768309f6762
seoohyeon <fltk0825@naver.com>
2023-05-12 00:42:04 +0900

    [feat] 뉴스 더미 데이터 생성

--- react-project/src/components/NewsForm/NewsList.tsx
+++ react-project/src/components/NewsForm/NewsList.tsx
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react';
 import NewsItem from './NewsItem';
-import axios from 'axios';
 import { LoadingSpinner } from '@/styles/LoadingStyled';
 import loadingImg from '/public/assets/loading.svg';
 import { NewsListBlock } from './NewsFormStyled';
 
+import newsData from '@/news.json';
+
 export default function NewsList() {
-  const [articles, setArticles] = useState(null);
+  const [articles, setArticles] = useState(newsData);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [displayedArticles, setDisplayedArticles] = useState([]);
-  const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
   const handleScroll = () => {
     const scrollHeight = document.documentElement.scrollHeight;
@@ -21,25 +21,6 @@ export default function NewsList() {
       setPage((prev) => prev + 1);
     }
   };
-
-  const getRandomImageThenSet = async () => {
-    setLoading(true);
-    try {
-      const response = await axios.get(
-        `${
-          import.meta.env.VITE_NEWS_API_URL
-        }/everything?q=bitcoin&apiKey=${API_KEY}&page=${page}`
-      );
-      setArticles(response.data.articles);
-    } catch {
-      console.error('fetching error');
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getRandomImageThenSet();
-  }, [page]);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
